6df7d8b9ab34af64d56bbd1c700f44743ab139b8
Yassine ELAOUNI <80796018+Yassine-deepth@users.noreply.github.com>
2023-11-01 21:23:11 +0100

    OG-2 add document of project

---
+++ Ducoments_de_Menicrat/nature.tsx
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="nature" tilewidth="32" tileheight="32" tilecount="180" columns="20">
+ <image source="RPG Nature Tileset.png" trans="000000" width="641" height="288"/>
+ <wangsets>
+  <wangset name="sho" type="mixed" tile="99">
+   <wangcolor name="" color="#ff0000" tile="96" probability="1"/>
+   <wangtile tileid="78" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="79" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="98" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="99" wangid="1,1,1,1,1,1,1,1"/>
+  </wangset>
+  <wangset name="pi" type="mixed" tile="75">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="43" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="75" wangid="1,1,0,0,1,1,1,1"/>
+   <wangtile tileid="76" wangid="1,1,0,0,0,0,0,1"/>
+   <wangtile tileid="77" wangid="1,1,1,1,0,0,0,1"/>
+   <wangtile tileid="95" wangid="0,0,0,0,0,1,1,1"/>
+   <wangtile tileid="97" wangid="0,1,1,1,0,0,0,0"/>
+   <wangtile tileid="115" wangid="0,0,1,1,1,1,1,1"/>
+   <wangtile tileid="116" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="117" wangid="0,1,1,1,1,1,1,0"/>
+  </wangset>
+  <wangset name="iiii" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="99" probability="1"/>
+   <wangtile tileid="43" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="78" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="79" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="98" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="99" wangid="0,1,0,1,0,1,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
